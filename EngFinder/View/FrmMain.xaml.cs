7ca4afb3cff3c8db7f4b5ba899b5a63a8c2cdd73
ENGworks Dev <pablo.derendinger@engworks.com>
2019-04-19 14:43:40 -0300

    Fixed bug #4

--- EngFinder/View/FrmMain.xaml.cs
+++ EngFinder/View/FrmMain.xaml.cs
@@ -118,7 +118,7 @@ namespace EngFinder.View
 
             try
             {
-                ObservableCollection<RevitParameter> vRevitParameters = new ObservableCollection<RevitParameter>(_RevitParameter.Where(p => String.Equals( p.Name, TxtSearch.Text, StringComparison.OrdinalIgnoreCase)));
+                ObservableCollection<RevitParameter> vRevitParameters = new ObservableCollection<RevitParameter>(_RevitParameter.Where(p => p.Name.ToLower().Contains( TxtSearch.Text.ToLower())));
                 ListParameterRefresh(vRevitParameters);
             }
             catch (Exception vEx)

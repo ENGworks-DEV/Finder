1a4da2947295f2075fb1c37338510609a7bc1a9e
joelpereira <armypereira@hotmail.com>
2019-05-27 13:10:27 -0300

    adjustments by refactorizaicon, assignment of events to send binary to revti during the debugging

--- EngFinder/Core/LibElement.cs
+++ EngFinder/Core/LibElement.cs
@@ -27,41 +27,41 @@ namespace EngFinder.Core
                 names.Add(it.Key.Name);
             }
             var parameter = _Doc.GetElement(valRevitParameter.ElementId);
-     
 
-            foreach ( var vCategoryId in valCategoryElementId)
+            LibNumeric insLibNumeric = new LibNumeric();
+            if (insLibNumeric.IsDouble(valValue))
+            {
+                vResult = GetElementValueDouble(valRevitParameter, valCategoryElementId, valValue);
+            }
+            else
+            {
+                vResult = GetElementValueIntOrstring(valRevitParameter, valCategoryElementId, valValue);
+            }
+
+                
+            return vResult;
+        }
+
+        private IList<Element> GetElementValueIntOrstring(RevitParameter valRevitParameter, List<ElementId> valCategoryElementId, string valValue)
+        {
+            IList<Element> vResult = new List<Element>();
+            foreach (var vCategoryId in valCategoryElementId)
             {
                 IList<ElementFilter> vList = new List<ElementFilter>();
-
                 BuiltInCategory vBuiltInCategory = (BuiltInCategory)vCategoryId.IntegerValue;
-                ParameterValueProvider vPovider  = new ParameterValueProvider(valRevitParameter.ElementId);
-                
+                ParameterValueProvider vPovider = new ParameterValueProvider(valRevitParameter.ElementId);
                 string vRulestring = valValue;
-                FilterStringRuleEvaluator vEvaluator  = new FilterStringEquals();
-                
+                FilterStringRuleEvaluator vEvaluator = new FilterStringEquals();
                 FilteredElementCollector vCollector = new FilteredElementCollector(_Doc);
                 vCollector.OfCategory(vBuiltInCategory);
-                FilterRule vRuleRulestring = new FilterStringRule( vPovider, vEvaluator, vRulestring, false);
+                FilterRule vRuleRulestring = new FilterStringRule(vPovider, vEvaluator, vRulestring, false);
                 ElementParameterFilter vFilterRulestring = new ElementParameterFilter(vRuleRulestring);
                 LibNumeric insLibNumeric = new LibNumeric();
-                if (insLibNumeric.IsDouble(valValue))
-                {
-                    Double vNum = 0;
-                    Double.TryParse(valValue, out vNum);
-                    FilterNumericGreater vEvaluatorNumeri = new FilterNumericGreater();
-                    double vRuleDoubleVal = vNum;
-                    const double eps = 10E-06;
-                    var vFilterDoubleRule = new FilterDoubleRule(vPovider, vEvaluatorNumeri, vRuleDoubleVal, eps);
-                    var vElementParameterFilterDoubleRule = new ElementParameterFilter(vFilterDoubleRule);
-                    vList.Add(vElementParameterFilterDoubleRule);
-                }
-
                 if (insLibNumeric.IsInt(valValue))
                 {
                     int vNum = 0;
                     int.TryParse(valValue, out vNum);
                     FilterNumericEquals vEvaluatorNumeri = new FilterNumericEquals();
-                    
                     int vRuleIntVal = vNum;
                     var vFilterIntRule = new FilterIntegerRule(vPovider, vEvaluatorNumeri, vRuleIntVal);
                     var vElementParameterFilterIntRule = new ElementParameterFilter(vFilterIntRule);
@@ -71,7 +71,54 @@ namespace EngFinder.Core
                 LogicalOrFilter vLogicalOrFilter = new LogicalOrFilter(vList);
                 vCollector.WherePasses(vLogicalOrFilter);
                 IList<Element> vElements = vCollector.ToElements();
-                vResult= vElements.Concat(vResult).ToList();
+                if (vElements != null)
+                {
+                    if (vElements.Count > 0)
+                    {
+                        vResult = vElements.Concat(vElements).ToList();
+
+                    }
+
+                }
+                
+            }
+            return vResult;
+        }
+
+        public IList<Element> GetElementValueDouble(RevitParameter valRevitParameter, List<ElementId> valCategoryElementId, string valValue)
+        {
+            IList<Element> vResult = new List<Element>();
+            foreach (var vCategoryId in valCategoryElementId)
+            {
+                BuiltInCategory vBuiltInCategory = (BuiltInCategory)vCategoryId.IntegerValue;
+                ParameterValueProvider vPovider = new ParameterValueProvider(valRevitParameter.ElementId);
+                string vRulestring = valValue;
+                FilteredElementCollector vCollector = new FilteredElementCollector(_Doc);
+                vCollector.OfCategory(vBuiltInCategory);
+                LibNumeric insLibNumeric = new LibNumeric();
+                double ruleValDb = 0.0;
+                if (insLibNumeric.IsDouble(valValue))
+                {
+                    Double vNum = 0;
+                    Double.TryParse(valValue, out vNum);
+                    ruleValDb = vNum;
+                }
+                ParameterValueProvider pvp = new ParameterValueProvider(valRevitParameter.ElementId);
+                FilterNumericEquals fnrv = new FilterNumericEquals();
+                var vFilterDoubleRule = new FilterDoubleRule(pvp, fnrv, ruleValDb, 10e-10);
+                var epf = new ElementParameterFilter(vFilterDoubleRule);
+                vCollector.WherePasses(epf);
+                IList<Element> vElements = vCollector.ToElements();
+                if (vElements != null)
+                {
+                    if (vElements.Count > 0)
+                    {
+                        vResult = vElements.Concat(vElements).ToList();
+
+                    }
+
+                }
+
             }
             return vResult;
         }

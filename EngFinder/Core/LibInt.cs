88e0b5dddc91ad9bf65752485137df43f474a23e
vnoves <novesvalentin@gmail.com>
2020-05-19 10:01:00 -0300

    Latest format changes

---
+++ EngFinder/Core/LibInt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngFinder.Core {
+    public class LibNumeric {
+
+        public bool IsDouble(string valText) {
+            Double vNum = 0;
+            bool vResult = false;
+            if (string.IsNullOrEmpty(valText)) {
+                return false;
+            }
+            vResult = Double.TryParse(valText, out vNum);
+            return vResult;
+        }
+
+        public bool IsInt(string valText) {
+            int vNum = 0;
+            bool vResult = false;
+            if (string.IsNullOrEmpty(valText)) {
+                return false;
+            }
+            vResult = int.TryParse(valText, out vNum);
+            return vResult;
+        }
+    }
+}
